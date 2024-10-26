8a33d99d5b1b957b7f5c2d7253b0daa854ece13b
yuna <micha0215@tukorea.ac.kr>
2024-10-26 20:59:47 +0900

    Update build.yml

--- Backend/src/test/java/com/pathsnap/Backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/pathsnap/Backend/BackendApplicationTests.java
@@ -3,11 +3,18 @@ package com.pathsnap.Backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void simpleMathTest() {
+		assertEquals(2, 1 + 1, "1 + 1은 2여야 합니다.");
 	}
 
 }
