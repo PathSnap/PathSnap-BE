4560126571baa440c37d7aeea34993d071d64183
김람운 <110807323+ramun2@users.noreply.github.com>
2024-10-28 15:17:23 +0900

    테이블 mapped by 추가

--- Backend/src/main/java/com/pathsnap/Backend/User/domain/UserEntity.java
+++ Backend/src/main/java/com/pathsnap/Backend/User/domain/UserEntity.java
@@ -1,22 +1,27 @@
 package com.pathsnap.Backend.User.domain;
 
 import com.pathsnap.Backend.Image.domain.ImageEntity;
+import com.pathsnap.Backend.PhotoRecord.domain.PhotoRecordEntity;
+import com.pathsnap.Backend.Record.domain.RecordEntity;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "User")
 @Data
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;
 
     @OneToOne
     @JoinColumn(name = "image_id")
     private ImageEntity image;
+
+    @OneToMany(mappedBy = "user")  // 양방향 관계 설정
+    private List<RecordEntity> records;
 
     private String phoneNumber;
     private Date birthDate;
