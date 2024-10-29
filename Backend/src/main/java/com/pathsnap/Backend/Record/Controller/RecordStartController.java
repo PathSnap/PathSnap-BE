ca2f3cfcaefa684e17c0d361b7be39e93734016f
김람운 <110807323+ramun2@users.noreply.github.com>
2024-10-29 20:32:45 +0900

    map함수 dto로 변환

--- Backend/src/main/java/com/pathsnap/Backend/Record/Controller/RecordStartController.java
+++ Backend/src/main/java/com/pathsnap/Backend/Record/Controller/RecordStartController.java
@@ -1,5 +1,6 @@
 package com.pathsnap.Backend.Record.Controller;
 
+import com.pathsnap.Backend.Record.Dto.Get.Res.RecordStartDTO;
 import com.pathsnap.Backend.Record.Service.RecordStartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController //json 형식으로 반환
 @RequestMapping("/records")
@@ -19,11 +17,12 @@ public class RecordStartController {
     RecordStartService recordStartService;
 
     @GetMapping("/start/{userId}/{recordIsGroup}")
-    public ResponseEntity<Map<String,String>> startRecord(@PathVariable String userId,@PathVariable boolean recordIsGroup) {
+    public ResponseEntity<RecordStartDTO> startRecord(@PathVariable String userId, @PathVariable boolean recordIsGroup) {
         String recordId = recordStartService.startNewRecord(userId,recordIsGroup);
 
-        Map<String,String> response = new HashMap<>();
-        response.put("recordId",recordId);
+        RecordStartDTO response = RecordStartDTO.builder()
+                .recordId(recordId)
+                .build();
 
         return ResponseEntity.ok(response);
     }
