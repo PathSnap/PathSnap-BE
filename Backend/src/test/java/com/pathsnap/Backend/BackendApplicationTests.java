141bec402b8c5d746c0812bc36ea56672d803ff8
yuna <micha0215@tukorea.ac.kr>
2024-10-31 19:16:24 +0900

    코드 정리

--- Backend/src/test/java/com/pathsnap/Backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/pathsnap/Backend/BackendApplicationTests.java
@@ -1,32 +1,18 @@
 package com.pathsnap.Backend;
 
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.pathsnap.Backend.Image.Service.ImageService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.mockito.Mock;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@MockBean
-	private AmazonS3 amazonS3; // AWS 클라이언트 모킹
-
-	@Autowired
-	private ImageService imageService;
-
 	@Test
 	void contextLoads() {
-		// AWS 관련 예외 방지를 위해 모킹 설정
-		when(amazonS3.putObject(anyString(), anyString(), any(), any())).thenReturn(null);
 	}
 
 	@Test
