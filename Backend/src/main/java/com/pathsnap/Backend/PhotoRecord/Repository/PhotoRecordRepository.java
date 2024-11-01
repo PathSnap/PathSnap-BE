a7c94da5cc7459bb8d4e13dcd4af14ed47db7eca
김람운 <110807323+ramun2@users.noreply.github.com>
2024-11-01 18:05:21 +0900

    포토기록 Repository 어노테이션 추가

--- Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Repository/PhotoRecordRepository.java
+++ Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Repository/PhotoRecordRepository.java
@@ -1,2 +1,10 @@
-package com.pathsnap.Backend.PhotoRecord.Repository;public interface PhotoRecordRepository {
+package com.pathsnap.Backend.PhotoRecord.Repository;
+
+import com.pathsnap.Backend.PhotoRecord.Entity.PhotoRecordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRecordRepository extends JpaRepository<PhotoRecordEntity,String> {
+
 }
