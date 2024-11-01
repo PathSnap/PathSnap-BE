217f20370aeab3534b644cb2f16fb27a8b4200a5
김람운 <110807323+ramun2@users.noreply.github.com>
2024-11-01 18:04:36 +0900

    포토기록 수정 controller 구현과 api 수정

--- Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Controller/PhotoRecordController.java
+++ Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Controller/PhotoRecordController.java
@@ -2,9 +2,12 @@ package com.pathsnap.Backend.PhotoRecord.Controller;
 
 import com.pathsnap.Backend.PhotoRecord.Dto.Req.PhotoRecordReqDto;
 import com.pathsnap.Backend.PhotoRecord.Dto.Res.PhotoRecordResDto;
-import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService;
+import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService.CreatePhotoService;
+import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService.UpdatePhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,12 +15,25 @@ import org.springframework.web.bind.annotation.*;
 public class PhotoRecordController {
 
     @Autowired
-    private PhotoRecordService photoRecordService;
+    @Qualifier("createPhotoService") // 주입할 빈 이름과 일치
+    private CreatePhotoService photoRecordService;
 
-    @PostMapping("/{recordId}")
+    @Autowired
+    @Qualifier("updatePhotoService")
+    private UpdatePhotoService updatePhotoService;
+
+    @PostMapping("/create/{recordId}")
     public ResponseEntity<PhotoRecordResDto> addPhoto(@PathVariable String recordId,
                                                       @RequestBody PhotoRecordReqDto request) {
         PhotoRecordResDto response = photoRecordService.createPhoto(recordId, request);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<PhotoRecordResDto> updatePhoto(@RequestBody PhotoRecordReqDto request){
+
+        PhotoRecordResDto response = updatePhotoService.updatePhoto(request);
         return ResponseEntity.ok(response);
 
     }
