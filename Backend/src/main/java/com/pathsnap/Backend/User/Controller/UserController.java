0757d1526ac951f8491461a0226af50e11f0f54d
yuna <micha0215@tukorea.ac.kr>
2024-11-01 18:40:29 +0900

    response 코드 수정

--- Backend/src/main/java/com/pathsnap/Backend/User/Controller/UserController.java
+++ Backend/src/main/java/com/pathsnap/Backend/User/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.pathsnap.Backend.User.Controller;
 import com.pathsnap.Backend.User.Dto.Res.UserResDTO;
 import com.pathsnap.Backend.User.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,8 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{userId}")
-    public UserResDTO getProfile(@PathVariable String userId) {
-        return userService.getProfile(userId);
+    public ResponseEntity<UserResDTO> getProfile(@PathVariable String userId) {
+        UserResDTO response = userService.getProfile(userId);
+        return ResponseEntity.ok(response);
     }
 }
