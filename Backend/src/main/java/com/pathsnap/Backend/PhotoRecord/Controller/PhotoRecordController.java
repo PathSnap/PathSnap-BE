6662c35cbcc55322706c64b63e33473fe183d1f1
김람운 <110807323+ramun2@users.noreply.github.com>
2024-11-03 22:52:20 +0900

    기본 홈 조회 기능 구현

--- Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Controller/PhotoRecordController.java
+++ Backend/src/main/java/com/pathsnap/Backend/PhotoRecord/Controller/PhotoRecordController.java
@@ -1,14 +1,17 @@
 package com.pathsnap.Backend.PhotoRecord.Controller;
 
 import com.pathsnap.Backend.PhotoRecord.Dto.Req.PhotoRecordReqDto;
+import com.pathsnap.Backend.PhotoRecord.Dto.Res.PhotoLocationResDto;
 import com.pathsnap.Backend.PhotoRecord.Dto.Res.PhotoRecordResDto;
 import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService.CreatePhotoService;
+import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService.GetPhotoLocationService;
 import com.pathsnap.Backend.PhotoRecord.Service.PhotoRecordService.UpdatePhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/photos")
@@ -22,6 +25,10 @@ public class PhotoRecordController {
     @Qualifier("updatePhotoService")
     private UpdatePhotoService updatePhotoService;
 
+    @Autowired
+    @Qualifier("getPhotoLocationService")
+    private GetPhotoLocationService getLocationService;
+
     @PostMapping("/create/{recordId}")
     public ResponseEntity<PhotoRecordResDto> addPhoto(@PathVariable String recordId,
                                                       @RequestBody PhotoRecordReqDto request) {
@@ -31,11 +38,20 @@ public class PhotoRecordController {
     }
 
     @PutMapping("/edit")
-    public ResponseEntity<PhotoRecordResDto> updatePhoto(@RequestBody PhotoRecordReqDto request){
+    public ResponseEntity<PhotoRecordResDto> updatePhoto(@RequestBody PhotoRecordReqDto request) {
 
         PhotoRecordResDto response = updatePhotoService.updatePhoto(request);
         return ResponseEntity.ok(response);
 
     }
 
+    @GetMapping("/{userId}/{lon}/{lat}/{radius}")
+    public ResponseEntity<List<PhotoLocationResDto>> getPhotosWithinRadius(
+            @PathVariable String userId,
+            @PathVariable double lon,
+            @PathVariable double lat,
+            @PathVariable double radius) {
+        List<PhotoLocationResDto> response = getLocationService.getPhotosWithinRadius(userId, lon, lat, radius);
+        return ResponseEntity.ok(response);
+    }
 }
