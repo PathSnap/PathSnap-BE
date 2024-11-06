5ee0d43fdf2a6ca38aecb4bca5012c7d801b6428
김람운 <110807323+ramun2@users.noreply.github.com>
2024-11-06 17:28:49 +0900

    Swagger 세팅

---
+++ Backend/src/main/java/com/pathsnap/Backend/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.pathsnap.Backend;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("PathSnap Swagger")
+                .description("PathSnap REST API")
+                .version("1.0.0");
+    }
+
+}
+
