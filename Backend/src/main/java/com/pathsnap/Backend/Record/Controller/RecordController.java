2de0eae5e8abf7d6ee47bed020170032cd631594
김람운 <110807323+ramun2@users.noreply.github.com>
2024-11-06 17:29:47 +0900

    Swagger 기록 controller 인터페이스 분리

--- Backend/src/main/java/com/pathsnap/Backend/Record/Controller/RecordController.java
+++ Backend/src/main/java/com/pathsnap/Backend/Record/Controller/RecordController.java
@@ -10,6 +10,8 @@ import com.pathsnap.Backend.Record.Service.RecordDetailService;
 import com.pathsnap.Backend.Record.Service.RecordEditService;
 import com.pathsnap.Backend.Record.Service.RecordStartService;
 import com.pathsnap.Backend.Record.Service.RecordUpdateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +20,7 @@ import java.util.List;
 
 @RestController //json 형식으로 반환
 @RequestMapping("/records")
-public class RecordController {
+public class RecordController implements RecordControllerDocs{
 
     @Autowired
     RecordStartService recordStartService;
@@ -40,6 +42,7 @@ public class RecordController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "기록 상세 조회", description = "피라미터로 받은 기록으로 기록 관련 정보들을 전달")
     @GetMapping("/detail/{recordId}")
     public ResponseEntity<RecordDetailResDto> getRecordDetail(@PathVariable String recordId) {
 
@@ -47,13 +50,14 @@ public class RecordController {
 
         return ResponseEntity.ok(response);
     }
-
+    @Operation(summary = "기록 순서 변경", description = "바디로 받은 순서가 변경된 포토기록과 경로기록을 저장하고 변경된 정보들 전달 ")
     @PutMapping("/update")
     public ResponseEntity<RecordUpdateResDto> updateRecordDetails(@RequestBody RecordUpdateReqDto request) {
 
         return ResponseEntity.ok(recordUpdateService.updateRecordDetails(request));
     }
 
+    @Operation(summary = "기록 이름 수정", description = "바디로 받은 변경된 기록이름을 저장하고 변경된 정보들을 전달")
     @PostMapping("/edit")
     public ResponseEntity<RecordEditResDto> editRecordName(@RequestBody RecordEditReqDto request){
         return ResponseEntity.ok(recordEditService.editRecordName(request));
