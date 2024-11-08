801c5e60607cf046fa0c78e4b2308ab4ac6391aa
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-11-08 22:48:21 +0900

    엔티티 매핑 오류 수정 2

--- Backend/src/main/java/com/pathsnap/Backend/Coordinate/Component/CreateCoordinate.java
+++ Backend/src/main/java/com/pathsnap/Backend/Coordinate/Component/CreateCoordinate.java
@@ -1,16 +1,16 @@
 package com.pathsnap.Backend.Coordinate.Component;
 
 import com.pathsnap.Backend.Coordinate.Dto.Req.CoordinateReqDto;
-import com.pathsnap.Backend.Coordinate.Entitiy.CoordinateEntity;
+import com.pathsnap.Backend.Coordinate.Entitiy.Coordinate1Entity;
 import com.pathsnap.Backend.RouteRecord.Entity.RouteRecord1Entity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateCoordinate {
-    public CoordinateEntity exec(CoordinateReqDto coordinateReqDto, RouteRecord1Entity routeRecord) {
+    public Coordinate1Entity exec(CoordinateReqDto coordinateReqDto, RouteRecord1Entity routeRecord) {
 
-        //CoordinateEntity 생성
-        return CoordinateEntity.builder()
+        //Coordinate1Entity 생성
+        return Coordinate1Entity.builder()
                 .lat(coordinateReqDto.getLat())
                 .lng(coordinateReqDto.getLng())
                 .timeStamp(coordinateReqDto.getTimeStamp())
--- Backend/src/main/java/com/pathsnap/Backend/Coordinate/Repository/CoordinateRepository.java
+++ Backend/src/main/java/com/pathsnap/Backend/Coordinate/Repository/CoordinateRepository.java
@@ -1,12 +1,10 @@
 package com.pathsnap.Backend.Coordinate.Repository;
 
-import com.pathsnap.Backend.Coordinate.Entitiy.CoordinateEntity;
+import com.pathsnap.Backend.Coordinate.Entitiy.Coordinate1Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CoordinateRepository extends JpaRepository<CoordinateEntity,String> {
+public interface CoordinateRepository extends JpaRepository<Coordinate1Entity,String> {
 
 }
