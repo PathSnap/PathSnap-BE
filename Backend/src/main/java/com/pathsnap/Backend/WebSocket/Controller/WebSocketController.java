6a599efde31d6e3c5a70fff616c9423499bec9bd
yuna <micha0215@tukorea.ac.kr>
2024-11-17 15:39:18 +0900

    log 삭제

--- Backend/src/main/java/com/pathsnap/Backend/WebSocket/Controller/WebSocketController.java
+++ Backend/src/main/java/com/pathsnap/Backend/WebSocket/Controller/WebSocketController.java
@@ -4,8 +4,6 @@ import com.pathsnap.Backend.RouteRecord.Dto.Res.RouteRecordStartDto;
 import com.pathsnap.Backend.RouteRecord.Service.CreateRouteRecordService;
 import com.pathsnap.Backend.WebSocket.WebSocketService.WebSocketAuthService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -19,12 +17,10 @@ public class WebSocketController {
     private final WebSocketAuthService webSocketAuthService;
     private final CreateRouteRecordService createRouteRecordService;
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private static final Logger log = LoggerFactory.getLogger(WebSocketController.class);
 
     // route 생성
     @MessageMapping("/routes/start/{userId}/{recordId}/{seq}")
     public void CreateRoute(@DestinationVariable String userId, @DestinationVariable String recordId, @DestinationVariable Integer seq) {
-
         // userId 확인
         webSocketAuthService.webSocketUser(userId, recordId);
 
@@ -32,7 +28,6 @@ public class WebSocketController {
         RouteRecordStartDto response = createRouteRecordService.startRoute(recordId,seq);
 
         simpMessagingTemplate.convertAndSend("/sub/routes/" + recordId, response);
-        log.info("Message sent to routeId: {}", response.getRouteId());
     }
 
 
