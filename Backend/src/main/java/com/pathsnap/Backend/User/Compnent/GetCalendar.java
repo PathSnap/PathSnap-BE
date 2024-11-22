ace13fd392f21b88d745ee2bfcac9b3346cc3d06
yuna <micha0215@tukorea.ac.kr>
2024-11-22 22:07:26 +0900

    대표이미지 불러오기 수정 완료

--- Backend/src/main/java/com/pathsnap/Backend/User/Compnent/GetCalendar.java
+++ Backend/src/main/java/com/pathsnap/Backend/User/Compnent/GetCalendar.java
@@ -2,7 +2,7 @@ package com.pathsnap.Backend.User.Compnent;
 
 import com.pathsnap.Backend.Image.Dto.Res.ImageResDto;
 import com.pathsnap.Backend.Record.Entity.Record1Entity;
-import com.pathsnap.Backend.User.Compnent.Small.GetUserLocationImage;
+import com.pathsnap.Backend.User.Compnent.Small.GetUserMainImage;
 import com.pathsnap.Backend.User.Dto.Res.CalendarResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,13 +13,13 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class GetCalendar {
-    private final GetUserLocationImage getUserLocationImage;
+    private final GetUserMainImage getUserMainImage;
 
     // CalendarResDto.CalendarDto 리스트 반환 메서드
     public List<CalendarResDto.CalendarDto> exec(List<Record1Entity> records) {
         List<CalendarResDto.CalendarDto> calendarDtos = new ArrayList<>();
         for (Record1Entity record : records) {
-            ImageResDto ImageResDto = getUserLocationImage.exec(record.getRecordId());
+            ImageResDto ImageResDto = getUserMainImage.exec(record.getRecordId());
             CalendarResDto.CalendarDto calendarDto = new CalendarResDto.CalendarDto(
                     record.getRecordId(),
                     record.getStartDate(),
