42ad90aa4953df7ea88bc75066de23f1760081c6
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-11-26 22:45:53 +0900

    JWTFilter 수정

--- Backend/src/main/java/com/pathsnap/Backend/Config/SecurityConfig.java
+++ Backend/src/main/java/com/pathsnap/Backend/Config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -75,7 +76,7 @@ public class SecurityConfig {
 
         //JwtFilter 추가
         http
-                .addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
+                .addFilterBefore(new JwtFilter(jwtUtil), SecurityContextPersistenceFilter.class);
 
         //oauth2
         http
