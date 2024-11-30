35f7110fd492c528099a58df35b07a47b0f6b333
yuna <micha0215@tukorea.ac.kr>
2024-11-30 17:29:12 +0900

    프로필 정보 예외처리 완료

--- Backend/src/main/java/com/pathsnap/Backend/User/Compnent/UpdateUserProfile.java
+++ Backend/src/main/java/com/pathsnap/Backend/User/Compnent/UpdateUserProfile.java
@@ -15,10 +15,29 @@ public class UpdateUserProfile {
 
     // 사용자 정보 업데이트 메서드
     public void exec(User1Entity user, UserUpdateReqDto userUpdateReqDto) {
-        user.setUserName(userUpdateReqDto.getUserName());
-        user.setBirthDate(userUpdateReqDto.getBirthDate());
-        user.setPhoneNumber(userUpdateReqDto.getPhoneNumber());
-        user.setAddress(userUpdateReqDto.getAddress());
+        if (userUpdateReqDto.getUserName() != null) {
+            user.setUserName(userUpdateReqDto.getUserName());
+        } else {
+            throw new IllegalArgumentException("UserName은 null일 수 없습니다.");
+        }
+
+        if (userUpdateReqDto.getBirthDate() != null) {
+            user.setBirthDate(userUpdateReqDto.getBirthDate());
+        } else {
+            throw new IllegalArgumentException("BirthDate는 null일 수 없습니다.");
+        }
+
+        if (userUpdateReqDto.getPhoneNumber() != null) {
+            user.setPhoneNumber(userUpdateReqDto.getPhoneNumber());
+        } else {
+            throw new IllegalArgumentException("PhoneNumber는 null일 수 없습니다.");
+        }
+
+        if (userUpdateReqDto.getAddress() != null) {
+            user.setAddress(userUpdateReqDto.getAddress());
+        } else {
+            throw new IllegalArgumentException("Address는 null일 수 없습니다.");
+        }
         if (userUpdateReqDto.getImageId() != null) {
             Image1Entity image = imageCheck.exec(userUpdateReqDto.getImageId());
             user.setImage(image);
